80c2e110452769bcd126b8d3e7c72cd2a80135e1
Xavier Trinidad <xmtrinidad@gmail.com>
2018-05-01 12:39:55 -0700

    Implemented delete, update, with views

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const app = express();
 
@@ -22,6 +23,9 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override middleware
+app.use(methodOverride('_method'))
+
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome';
@@ -33,10 +37,29 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Note Index Page
+app.get('/notes', (req, res) => {
+  Note.find({})
+    .sort({ date: 'desc' })
+    .then(notes => {
+      res.render('notes/index', { notes: notes });
+    });
+});
+
 // Add Note Form
 app.get('/notes/add', (req, res) => {
   res.render('notes/add');
 });
+
+// Edit Note Form
+app.get('/notes/edit/:id', (req, res) => {
+  Note.findOne({ _id: req.params.id })
+    .then(note => {
+      res.render('notes/edit', { note: note });
+    });
+
+});
+
 
 // Process Form
 app.post('/notes', (req, res) => {
@@ -57,9 +80,39 @@ app.post('/notes', (req, res) => {
         note: req.body.note
       });
   } else {
-    res.send('passed');
+    const newUser = {
+      title: req.body.title,
+      note: req.body.note
+    }
+    new Note(newUser)
+      .save()
+      .then(note => {
+        res.redirect('/notes');
+      });
   }
 });
+
+// Edit form process
+app.put('/notes/:id', (req, res) => {
+  Note.findOne({ _id: req.params.id })
+    .then(note => {
+      note.title = req.body.title;
+      note.note = req.body.note;
+      note.save()
+        .then(note => {
+          res.redirect('/notes');
+        })
+    })
+});
+
+// Delete Note
+app.delete('/notes/:id', (req, res) => {
+  Note.remove({ _id: req.params.id })
+    .then(() => {
+      res.redirect('/notes');
+    });
+})
+
 
 const port = 5000;
 
